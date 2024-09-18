a93e707103d7d04e68af194ea85c44e20b07fddf
sania <sislam1@tulane.edu>
2024-09-18 09:43:12 -0500

    9.18

--- # I want to add this file to my github r.py
+++ # I want to add this file to my github r.py
@@ -1,4 +1,11 @@
 # I want to add this file to my github repository
-print("everything bagel")
+#writing 8/26 notes here
+#write a function that adds two numbers
+def sum_function(int1, int2):
+    '''int1&int2 we want to add'''
+    print(int1 + int2)
+
+sum_function(5, 10)
+
 
 
---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+#Notes 8/26
+print("hello world")
+import numpy as np
+numpy_test = np.array([1,2,3])
+print(numpy_test)
